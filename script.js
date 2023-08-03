77c57adaa78034850e427aceffc3501bae45bffb
TroLyAmazon <congdongamazon@gmail.com>
2023-08-03 21:14:05 +0700

    Fix2131654+897

--- script.js
+++
@@ -1,9 +0,0 @@
-//Tìm Kiếm Nhanh
-function launchSearch(event) {
-    document.getElementById('searchBar___container').classList.add("activeSearch");
-  }
-  
-  document.getElementById('closeSearchBar').addEventListener('click', function() {
-    document.getElementById('searchBar___container').classList.remove("activeSearch");
-    
-  });
