4850a4d45c554501cb51cdd68b34c8bba0c2a98a
TroLyAmazon <congdongamazon@gmail.com>
2023-08-07 20:32:26 +0700

    update giao diện và fix tính năng

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Tạo một thời gian chờ ngẫu nhiên từ 5 đến 10 giây (trong miliseconds)
+const randomTimeout = Math.floor(Math.random() * 6000) + 5000;
+
+// Sử dụng setTimeout để chuyển hướng sau thời gian chờ ngẫu nhiên
 setTimeout(function() {
-    window.location.href = 'webchinhthuc/webchinhthuc.html';
-  }, 5000); // 5 seconds
+    window.location.href = 'webchinhthuc/index.html';
+}, randomTimeout);
