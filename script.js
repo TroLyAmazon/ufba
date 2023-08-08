60a942beb43b8b187c36d95102da3e7e0349bcd6
TroLyAmazon <congdongamazon@gmail.com>
2023-08-08 10:30:39 +0700

    F

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Tạo một thời gian chờ ngẫu nhiên từ 5 đến 10 giây (trong miliseconds)
-const randomTimeout = Math.floor(Math.random() * 6000) + 5000;
+// Tạo một thời gian chờ ngẫu nhiên từ 1 đến 10 giây (trong miliseconds)
+const randomTimeout = Math.floor(Math.random() * 6000) + 1000;
 
 // Sử dụng setTimeout để chuyển hướng sau thời gian chờ ngẫu nhiên
 setTimeout(function() {
