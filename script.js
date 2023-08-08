0ec82e367b68937254452663edfbbb36a944d949
TroLyAmazon <congdongamazon@gmail.com>
2023-08-08 20:43:17 +0700

    Hiệu Ứng

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-// Tạo một thời gian chờ ngẫu nhiên từ 1 đến 10 giây (trong miliseconds)
-const randomTimeout = Math.floor(Math.random() * 6000) + 1000;
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Calling Splitting()');
+    Splitting();
+});
+// Tạo một thời gian chờ ngẫu nhiên từ 3 đến 15 giây (trong miliseconds)
+const randomTimeout = Math.floor(Math.random() * 11000) + 3000;
 
-// Sử dụng setTimeout để chuyển hướng sau thời gian chờ ngẫu nhiên
-setTimeout(function() {
-    window.location.href = 'trangchu.html';
-}, randomTimeout);
-//Chữ Nhảy
-splitting();
+// Đợi thời gian chờ ngẫu nhiên và sau đó chuyển hướng trang
+    setTimeout(function() {
+        window.location.href = 'trangchu.html';
+    }, randomTimeout);
